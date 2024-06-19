9476104dd18a9786d726406afd009ab47b59f986
Desmondrattagan <drattagan@udesa.edu.ar>
2024-06-19 18:07:55 -0300

    fixes

--- my-app/App.js
+++ my-app/App.js
@@ -2,9 +2,9 @@ import {Text, View} from 'react-native';
 import MainNav from './src/navigation/MainNav'
 export default function App(){
     return (
-        <View>
+      
             <MainNav />
-            <Profile />
-        </View>
+            
+     
     )
 }
