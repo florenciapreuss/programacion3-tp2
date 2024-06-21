9d52867b0d1e48415e324ee59f8e8fae06b51775
AntoCastellanii2003 <antocastellani@udesa.edu.ar>
2024-06-21 10:12:22 -0300

    MyProfile

--- my-app/src/screens/MyProfile.js
+++ my-app/src/screens/MyProfile.js
@@ -1,45 +1,97 @@
-import { Text, View, TouchableOpacity } from 'react-native'
-import React, { Component } from 'react'
-import { auth } from '../firebase/config'
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image } from 'react-native';
+import { auth, db } from '../../firebase/config';
+import { getAuth, deleteUser } from "firebase/auth";
+import Posteo from '../../components/Posteo';
 
 class MyProfile extends Component {
-    constructor(props){
-        super(props)
+    constructor() {
+        super();
         this.state = {
-            estaLogueado: false
-        }
+            PostsUser: [],
+        };
     }
-    componentDidMount(){
-        auth.onAuthStateChanged((user) => {
-            if(user){
-                this.setState({estaLogueado: true})
-            }
-        })
+
+    componentDidMount() {
+        db.collection("posts").where("userPost", "==", auth.currentUser.email)
+            .onSnapshot((docs) => {
+                let postsDb = [];
+                docs.forEach(doc => {
+                    postsDb.push({
+                        id: doc.id,
+                        data: doc.data()
+                    });
+                });
+                console.log('Posts:', postsDb)
+                this.setState({
+                    PostsUser: postsDb
+                });
+            });
     }
-    cerrarSesion(){
+
+    logout() {
         auth.signOut()
-        .then(resp => console.log(resp))
-        .catch(err => console.log(err))
+            .then(() => {
+                this.props.navigation.navigate("login");
+            })
+            .catch(e => {
+                console.log(e);
+            });
     }
+
+
+    deleteThePost(idSpecificPost) {
+        db.collection("posts")
+            .doc(idSpecificPost)
+            .delete()
+            .then((res) => console.log(res))
+            .catch(e => console.log(e))
+
+    }
+
+    borrarUsuario(id) {
+        const SpecificUser = auth.currentUser;
+        db.collection('users').doc(id).delete()
+            .then(() => {
+                SpecificUser.delete()
+                    .then(() => {
+                        console.log('Removed User');
+                        this.props.navigation.navigate("login");
+                    })
+                    .catch((error) => {
+                        console.log('User could not be deleted', error);
+                    });
+            })
+            .catch((error) => {
+                console.log('Document not deleted', error);
+            });
+    }
+
     render() {
-    return (
-      <View>
-        <Text>My Profile</Text>
-        {
-            this.state.estaLogueado ? /* Si esta logueado es true, entonces puede cerrar sesion */
-            <TouchableOpacity
-                onPress={()=> this.cerrarSesion()}
-            >
-                <Text>Log out</Text>
-            </TouchableOpacity>
-            :
-            <TouchableOpacity>
-                <Text>Login</Text>
-            </TouchableOpacity>
-        }
-      </View>
-    )
-  }
+        return (
+            <View /* style={} */>
+                <Image /* style={} *//>
+                {
+                    this.state.PostsUser.length > 0 ?
+                        <FlatList
+                            data={this.state.PostsUser}
+                            keyExtractor={(item) => item.id.toString()}
+                            renderItem={({ item }) =>
+                         <View>  <Posteo borrarPosteo={(idPosteo) => this.deleteThePost(idPosteo)} post={item} /> </View>}
+                        />
+                        :
+                        <Text>This user does not have any posts</Text>
+                }
+                <TouchableOpacity /* style={} */ onPress={() => this.logout()}>
+                    <Text /* style={} */>LogOut</Text>
+                </TouchableOpacity>
+                <TouchableOpacity /* style={} */ onPress={() => (this.borrarUsuario())(this.props.navigation.navigate("Home"))}>
+                    <Text /* style={} */>Delete this Profile</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
-export default MyProfile
+
+export default MyProfile;
